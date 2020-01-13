9d3e67d3d68879a51471e8932288704dfc373b33
Vishal Gangurde <wizy100@gmail.com>
2020-01-13 11:01:03 +0530

    Updates

---
+++ precache-manifest.1b2fb237149cd0aec72ab5ed4453509a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a31694e42cc67737c745026e6cdd46",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0edae92ae66293bddf47",
+    "url": "/monsters-rolodex/static/css/main.67913297.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0edae92ae66293bddf47",
+    "url": "/monsters-rolodex/static/js/main.91b738dc.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
